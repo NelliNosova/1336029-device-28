17a3af6c205c8a2a37adc020d97b50d5280ab23d
Nelli Nosova <nellinosovapl@gmail.com>
2020-07-07 12:51:09 +0200

    Подогнала под тз и минимизированла файлы

--- js/script.js
+++ js/script.js
@@ -1,240 +1,243 @@
-var writeUs = document.querySelector(".write-us");
-var writeUsOpen = document.querySelector(".write-us-open");
-var writeUsClose = document.querySelector(".write-us-close");
-var map = document.querySelector(".map");
-var mapOpen = document.querySelector(".map-open");
-var mapClose = document.querySelector(".map-close");
-var writeUsForm = document.querySelector(".write-us-form");
-var login = document.querySelector("#name");
-var eMail = document.querySelector("#e-mail");
-var mailContent = document.querySelector("#mail-content");
-var one = document.querySelector(".one");
-var two = document.querySelector(".two");
-var three = document.querySelector(".three");
-var selfieStick = document.querySelector(".selfie-stick");
-var smartBand = document.querySelector(".smart-band");
-var quadcopter = document.querySelector(".quadcopter");
-var btnDelivery = document.querySelector(".btn-delivery");
-var btnGuarantee = document.querySelector(".btn-guarantee");
-var btnCredit = document.querySelector(".btn-credit"); 
-var serviceDelivery = document.querySelector(".service-delivery"); 
-var servicGuarantee = document.querySelector(".service-guarantee"); 
-var servicCredit = document.querySelector(".service-credit"); 
+(function () {
+  var writeUs = document.querySelector(".write-us");
+  var writeUsOpen = document.querySelector(".write-us-open");
+  var writeUsClose = document.querySelector(".write-us-close");
+  var map = document.querySelector(".map");
+  var mapOpen = document.querySelector(".map-open");
+  var mapClose = document.querySelector(".map-close");
+  var writeUsForm = document.querySelector(".write-us-form");
+  var login = document.querySelector("#name");
+  var eMail = document.querySelector("#e-mail");
+  var mailContent = document.querySelector("#mail-content");
+  var one = document.querySelector(".one");
+  var two = document.querySelector(".two");
+  var three = document.querySelector(".three");
+  var selfieStick = document.querySelector(".selfie-stick");
+  var smartBand = document.querySelector(".smart-band");
+  var quadcopter = document.querySelector(".quadcopter");
+  var btnDelivery = document.querySelector(".btn-delivery");
+  var btnGuarantee = document.querySelector(".btn-guarantee");
+  var btnCredit = document.querySelector(".btn-credit"); 
+  var serviceDelivery = document.querySelector(".service-delivery"); 
+  var servicGuarantee = document.querySelector(".service-guarantee"); 
+  var servicCredit = document.querySelector(".service-credit"); 
 
-var isStorageSupport = true;
-var loginItem = "";
-var eMailItem, childrenItem;
+  var isStorageSupport = true;
+  var loginItem = "";
+  var eMailItem, childrenItem;
 
-try {
+  try {
   loginItem = localStorage.getItem("login");
   eMailItem = localStorage.getItem("eMail");
-} catch (err) {
+  } catch (err) {
   isStorageSupport = false;
-}
+  }
 
-writeUsOpen.addEventListener("click", function (evt) {
+  writeUsOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   writeUs.classList.remove("none");
   login.value = loginItem;
   eMail.value = eMailItem;
   login.focus();
-});
+  });
 
-writeUsClose.addEventListener("click", function (evt) {
+  writeUsClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   writeUs.classList.add("none");
-});
+  });
 
-window.addEventListener("keydown", function (evt) {
+  window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (!writeUs.classList.contains("none")) {
-      evt.preventDefault();
-      writeUs.classList.add("none");
-    }
-
-    if (!map.classList.contains("none")) {
-      evt.preventDefault();
-      map.classList.add("none");
-    }
+  if (!writeUs.classList.contains("none")) {
+    evt.preventDefault();
+    writeUs.classList.add("none");
   }
-});
+
+  if (!map.classList.contains("none")) {
+    evt.preventDefault();
+    map.classList.add("none");
+  }
+  }
+  });
 
 
-writeUsForm.addEventListener("submit", function (evt) {
+  writeUsForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   evt.stopPropagation();
 
   if (isStorageSupport) {
-    localStorage.setItem("login", login.value);
-    localStorage.setItem("eMail", eMail.value);
+  localStorage.setItem("login", login.value);
+  localStorage.setItem("eMail", eMail.value);
   }
 
   if (!login.value || !eMail.value || !mailContent.value) {
-    writeUsForm.classList.add("write-us-error");
-    setTimeout(function () {
-      writeUsForm.classList.remove("write-us-error");
-    }, 600)
+  writeUsForm.classList.add("write-us-error");
+  setTimeout(function () {
+    writeUsForm.classList.remove("write-us-error");
+  }, 600)
   } else {
-    writeUsForm.submit();
+  writeUsForm.submit();
   }
 
   if (!login.value) {
-    login.classList.add("input-error");
-    login.focus();
+  login.classList.add("input-error");
+  login.focus();
   } else {
-    login.classList.remove("input-error");
-    eMail.focus();
+  login.classList.remove("input-error");
+  eMail.focus();
   }
 
   if (!eMail.value) {
-    eMail.classList.add("input-error");
+  eMail.classList.add("input-error");
   } else {
-    eMail.classList.remove("input-error");
-    mailContent.focus();
+  eMail.classList.remove("input-error");
+  mailContent.focus();
   }
 
   if (!mailContent.value) {
-    mailContent.classList.add("input-error");
+  mailContent.classList.add("input-error");
   } else {
-    mailContent.classList.remove("input-error");
+  mailContent.classList.remove("input-error");
   }
 
-});
+  });
 
-mapOpen.addEventListener("click", function (evt) {
+  mapOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   map.classList.remove("none");
-});
+  });
 
-mapClose.addEventListener("click", function (evt) {
+  mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   map.classList.add("none");
-});
+  });
 
 
-one.addEventListener("click", function (evt) {
+  one.addEventListener("click", function (evt) {
   if (!one.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    one.classList.add("promo-slider-active");
-    selfieStick.classList.remove("none");
+  evt.preventDefault();
+  one.classList.add("promo-slider-active");
+  selfieStick.classList.remove("none");
   }
 
   if (two.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    two.classList.remove("promo-slider-active");
-    smartBand.classList.add("none");
+  evt.preventDefault();
+  two.classList.remove("promo-slider-active");
+  smartBand.classList.add("none");
   }
 
   if (three.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    three.classList.remove("promo-slider-active");
-    quadcopter.classList.add("none");
+  evt.preventDefault();
+  three.classList.remove("promo-slider-active");
+  quadcopter.classList.add("none");
   }
 
-});
+  });
 
-two.addEventListener("click", function (evt) {
+  two.addEventListener("click", function (evt) {
   if (!two.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    two.classList.add("promo-slider-active");
-    smartBand.classList.remove("none");
+  evt.preventDefault();
+  two.classList.add("promo-slider-active");
+  smartBand.classList.remove("none");
   }
 
   if (one.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    one.classList.remove("promo-slider-active");
-    selfieStick.classList.add("none");
+  evt.preventDefault();
+  one.classList.remove("promo-slider-active");
+  selfieStick.classList.add("none");
   }
 
   if (three.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    three.classList.remove("promo-slider-active");
-    quadcopter.classList.add("none");
+  evt.preventDefault();
+  three.classList.remove("promo-slider-active");
+  quadcopter.classList.add("none");
   }
 
-}); 
+  }); 
 
-three.addEventListener("click", function (evt) {
+  three.addEventListener("click", function (evt) {
   if (!three.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    three.classList.add("promo-slider-active");
-    quadcopter.classList.remove("none");
+  evt.preventDefault();
+  three.classList.add("promo-slider-active");
+  quadcopter.classList.remove("none");
   }
 
   if (one.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    one.classList.remove("promo-slider-active");
-    selfieStick.classList.add("none");
+  evt.preventDefault();
+  one.classList.remove("promo-slider-active");
+  selfieStick.classList.add("none");
   }
 
   if (two.classList.contains("promo-slider-active")) {
-    evt.preventDefault();
-    two.classList.remove("promo-slider-active");
-    smartBand.classList.add("none");
+  evt.preventDefault();
+  two.classList.remove("promo-slider-active");
+  smartBand.classList.add("none");
   }
 
-});
+  });
 
 
-btnDelivery.addEventListener("click", function (evt) {
+  btnDelivery.addEventListener("click", function (evt) {
   if (!btnDelivery.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnDelivery.classList.add("services-active");
-    serviceDelivery.classList.remove("none");
+  evt.preventDefault();
+  btnDelivery.classList.add("services-active");
+  serviceDelivery.classList.remove("none");
   }
 
   if (btnGuarantee.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnGuarantee.classList.remove("services-active");
-    servicGuarantee.classList.add("none");
+  evt.preventDefault();
+  btnGuarantee.classList.remove("services-active");
+  servicGuarantee.classList.add("none");
   }
 
   if (btnCredit.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnCredit.classList.remove("services-active");
-    servicCredit.classList.add("none");
+  evt.preventDefault();
+  btnCredit.classList.remove("services-active");
+  servicCredit.classList.add("none");
   }
 
-});
+  });
 
-btnGuarantee.addEventListener("click", function (evt) {
+  btnGuarantee.addEventListener("click", function (evt) {
   if (!btnGuarantee.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnGuarantee.classList.add("services-active");
-    servicGuarantee.classList.remove("none");
+  evt.preventDefault();
+  btnGuarantee.classList.add("services-active");
+  servicGuarantee.classList.remove("none");
   }
 
   if (btnDelivery.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnDelivery.classList.remove("services-active");
-    serviceDelivery.classList.add("none");
+  evt.preventDefault();
+  btnDelivery.classList.remove("services-active");
+  serviceDelivery.classList.add("none");
   }
 
   if (btnCredit.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnCredit.classList.remove("services-active");
-    servicCredit.classList.add("none");
+  evt.preventDefault();
+  btnCredit.classList.remove("services-active");
+  servicCredit.classList.add("none");
   }
 
-}); 
+  }); 
 
-btnCredit.addEventListener("click", function (evt) {
+  btnCredit.addEventListener("click", function (evt) {
   if (!btnCredit.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnCredit.classList.add("services-active");
-    servicCredit.classList.remove("none");
+  evt.preventDefault();
+  btnCredit.classList.add("services-active");
+  servicCredit.classList.remove("none");
   }
 
   if (btnDelivery.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnDelivery.classList.remove("services-active");
-    serviceDelivery.classList.add("none");
+  evt.preventDefault();
+  btnDelivery.classList.remove("services-active");
+  serviceDelivery.classList.add("none");
   }
 
   if (btnGuarantee.classList.contains("services-active")) {
-    evt.preventDefault();
-    btnGuarantee.classList.remove("services-active");
-    servicGuarantee.classList.add("none");
+  evt.preventDefault();
+  btnGuarantee.classList.remove("services-active");
+  servicGuarantee.classList.add("none");
   }
 
-});
+  });
+
+})();
